d49a71ea9f7b586f1b2c0756fa14ce067729dfe3
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-04-12 02:38:39 -0400

    basic setup, gcd, print, mul, div

---
+++ rational.js
@@ -0,0 +1,36 @@
+// inspired by SICP book chapter 2 data abstractions
+
+// greatest common denominator -- Euclid's algorithm
+const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b))
+
+class Rat {
+	// store num and den as object properties
+	constructor(a, b) {
+		const g = gcd(a, b)
+
+		this.num = a / g
+		this.den = b / g
+	}
+
+	// mult, div, etc as static methods
+	static mult(x, y) {
+		return new Rat(x.num * y.num, x.den * y.den)
+	}
+
+	static div(x, y) {
+		return new Rat(x.num * y.den, y.num * x.den)
+	}
+
+	static print(x) {
+		return console.log(`${x.num}/${x.den}`)
+	}
+}
+
+// instantiante new Rats
+const r1 = new Rat(2, 5)
+const r2 = new Rat(5, 11)
+
+const testRat = Rat.div(r1, r2)
+
+// use print fn to print as Rat
+Rat.print(testRat)
